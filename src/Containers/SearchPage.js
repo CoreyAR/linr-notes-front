8f2ed6900ebf6c9b517fa9c2c20b747ae7abd125
Matt Kraatz <matt.kraatz@gmail.com>
2017-04-15 13:42:29 -0500

    commit

--- src/Containers/SearchPage.js
+++ src/Containers/SearchPage.js
@@ -1,12 +1,8 @@
 import React from 'react'
-import mapStyles from './Styles/RootMapStyle'
-import RaisedButton from 'material-ui/RaisedButton'
+import SearchForm from '../Components/SearchForm';
+import SearchResults from '../Components/SearchResults';
 
 class SearchPage extends React.Component {
-  getInitialState () {
-    return {
-    }
-  }
 
   componentWillReceiveProps (nextProps) {
   }
@@ -16,31 +12,11 @@ class SearchPage extends React.Component {
   }
 
   render () {
-    const style = {
-      position: 'absolute',
-      top: '0',
-      right: '0',
-      bottom: '0',
-      left: '0',
-      width: '100%',
-      height: '100%'
-    }
-
     return (
       <div>
         <h1>SearchPage</h1>
-        <form>
-          <label for="artistSearch">Artist</label>
-          <input type="text" name="artistSearch" id="artistSearch" />
-          <br/>
-          <label for="songSearch">Song</label>
-          <input type="text" name="songSearch" id="songSearch" />
-          <br/>
-          <label for="spotifyUriSearch">Spotify URI</label>
-          <input type="text" name="spotifyUriSearch" id="spotifyUriSearch" />
-          <br/>
-          <RaisedButton>Submit</RaisedButton>
-        </form>
+        <SearchForm />
+        <SearchResults />
       </div>
     )
   }
